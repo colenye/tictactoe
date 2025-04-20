f32c9e31ded65cc03ea6794b0a41c64d4f31ef4a
colenye <colenye529@gmail.com>
2025-04-20 03:47:00 -0400

    almost finished basic tictactoe, just can click on top of square when not supposed to

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { useState } from 'react';
 
 let turn = 'X';
+let table = [[null, null, null],[null, null, null],[null, null, null]]
 function Square( {row, column, getTic} ) {
   const [tic, setTic] = useState();
 
   function handleClick() {
-    getTic([row, column]);
+    
+    getTic([row, column, turn]);
     if (turn == 'X') {
       setTic('X');
       turn = 'O';
@@ -24,9 +26,9 @@ function Square( {row, column, getTic} ) {
 function Row( {row, getTic} ) {
   return (
     <div className="row">
+      <Square row={row} column={0} getTic={getTic}/>
       <Square row={row} column={1} getTic={getTic}/>
       <Square row={row} column={2} getTic={getTic}/>
-      <Square row={row} column={3} getTic={getTic}/>
     </div>
   )
 }
@@ -34,13 +36,30 @@ function Table() {
   const [globalTic, setGlobalTic] = useState();
   function getTic(tic){
     setGlobalTic(tic);
-    console.log(tic);
+    table[tic[0]][tic[1]] = tic[2];
+    console.log(table[0]);
+    console.log(table[1]);
+    console.log(table[2]);
+    for (let x = 0; x < 3; x++){
+      if (table[x][0] == table[x][1] && table[x][1] == table[x][2] && (table[x][2] == 'X' || table[x][2] == 'O')){
+        console.log("game over");
+      } 
+      if (table[0][x] == table[1][x] && table[1][x] == table[2][x] && (table[2][x] == 'X' || table[2][x] == 'O')){
+        console.log("game over");
+      }
+    }
+    if (table[0][0] == table[1][1] && table[1][1] == table[2][2] && (table[2][2] == 'X' || table[2][2] == 'O')){
+      console.log("game over");
+    } 
+    if (table[0][2] == table[1][1] && table[1][1] == table[2][0] && (table[2][0] == 'X' || table[2][0] == 'O')){
+      console.log("game over");
+    } 
   }
   return (
     <div className="table">
+      <Row row={0} getTic={getTic}/>
       <Row row={1} getTic={getTic}/>
       <Row row={2} getTic={getTic}/>
-      <Row row={3} getTic={getTic}/>
     </div>
   )
 }
