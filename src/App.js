d975b94bae4721d72e9f9ceb1eee1da1a7207013
colenye <colenye529@gmail.com>
2025-04-20 04:07:00 -0400

    bare bones tictactoe complete

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function Row( {row, getTic} ) {
     </div>
   )
 }
-function Table() {
+function Table( {makeGameOver} ) {
   const [globalTic, setGlobalTic] = useState();
   function getTic(tic){
     setGlobalTic(tic);
@@ -42,17 +42,33 @@ function Table() {
     console.log(table[2]);
     for (let x = 0; x < 3; x++){
       if (table[x][0] == table[x][1] && table[x][1] == table[x][2] && (table[x][2] == 'X' || table[x][2] == 'O')){
-        console.log("game over");
+        if(table[x][2] == 'X'){
+          makeGameOver('1');
+        } else{
+          makeGameOver('2');
+        }
       } 
       if (table[0][x] == table[1][x] && table[1][x] == table[2][x] && (table[2][x] == 'X' || table[2][x] == 'O')){
-        console.log("game over");
+        if(table[2][x] == 'X'){
+          makeGameOver('1');
+        } else{
+          makeGameOver('2');
+        }
       }
     }
     if (table[0][0] == table[1][1] && table[1][1] == table[2][2] && (table[2][2] == 'X' || table[2][2] == 'O')){
-      console.log("game over");
+      if(table[2][2] == 'X'){
+        makeGameOver('1');
+      } else{
+        makeGameOver('2');
+      }
     } 
     if (table[0][2] == table[1][1] && table[1][1] == table[2][0] && (table[2][0] == 'X' || table[2][0] == 'O')){
-      console.log("game over");
+      if(table[2][0] == 'X'){
+        makeGameOver('1');
+      } else{
+        makeGameOver('2');
+      }
     } 
   }
   return (
@@ -65,12 +81,23 @@ function Table() {
 }
 
 function App() {
+  const [gameOver, setGameOver] = useState();
+  function makeGameOver(player){
+    if (player == '1'){
+      setGameOver("https://y.yarn.co/69daa53a-7641-45ae-aea7-203c6b4a0e90_text.gif");
+    } else {
+      setGameOver("https://media.tenor.com/lGhAoEiUlTcAAAAM/player2wins.gif");
+    }
+
+  }
   return (
     <div className="App">
       <h1 className="">
         Tictac
       </h1>
-      <Table/>
+
+      <img src={gameOver} alt="suggon"/>
+      <Table makeGameOver = {makeGameOver} />
     </div>
   );
 }
