6cdfdec7bc4fa81bffe199463e185d78a76fe0dc
colenye <colenye529@gmail.com>
2025-04-20 02:55:28 -0400

    basic table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Square() {
+  return (
+    <button></button>
+  )
+}
+
+function Row() {
+  return (
+    <div className="row">
+      <Square/>
+      <Square/>
+      <Square/>
+    </div>
+  )
+}
+function Table() {
+  return (
+    <div className="table">
+      <Row/>
+      <Row/>
+      <Row/>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        Tictac
+      </h1>
+      <Table/>
+    </>
   );
 }
 
