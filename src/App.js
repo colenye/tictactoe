bdd8d0d706ba8af4b6675378f00b6b4f13a9dda1
colenye <colenye529@gmail.com>
2025-04-20 04:17:00 -0400

    removed warnings

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function Square( {row, column, getTic} ) {
   function handleClick() {
     if (tic != null) return;
     getTic([row, column, turn]);
-    if (turn == 'X') {
+    if (turn === 'X') {
       setTic('X');
       turn = 'O';
     } else {
@@ -41,30 +41,30 @@ function Table( {makeGameOver} ) {
     console.log(table[1]);
     console.log(table[2]);
     for (let x = 0; x < 3; x++){
-      if (table[x][0] == table[x][1] && table[x][1] == table[x][2] && (table[x][2] == 'X' || table[x][2] == 'O')){
-        if(table[x][2] == 'X'){
+      if (table[x][0] === table[x][1] && table[x][1] === table[x][2] && (table[x][2] === 'X' || table[x][2] === 'O')){
+        if(table[x][2] === 'X'){
           makeGameOver('1');
         } else{
           makeGameOver('2');
         }
       } 
-      if (table[0][x] == table[1][x] && table[1][x] == table[2][x] && (table[2][x] == 'X' || table[2][x] == 'O')){
-        if(table[2][x] == 'X'){
+      if (table[0][x] === table[1][x] && table[1][x] === table[2][x] && (table[2][x] === 'X' || table[2][x] === 'O')){
+        if(table[2][x] === 'X'){
           makeGameOver('1');
         } else{
           makeGameOver('2');
         }
       }
     }
-    if (table[0][0] == table[1][1] && table[1][1] == table[2][2] && (table[2][2] == 'X' || table[2][2] == 'O')){
-      if(table[2][2] == 'X'){
+    if (table[0][0] === table[1][1] && table[1][1] === table[2][2] && (table[2][2] === 'X' || table[2][2] === 'O')){
+      if(table[2][2] === 'X'){
         makeGameOver('1');
       } else{
         makeGameOver('2');
       }
     } 
-    if (table[0][2] == table[1][1] && table[1][1] == table[2][0] && (table[2][0] == 'X' || table[2][0] == 'O')){
-      if(table[2][0] == 'X'){
+    if (table[0][2] === table[1][1] && table[1][1] === table[2][0] && (table[2][0] === 'X' || table[2][0] === 'O')){
+      if(table[2][0] === 'X'){
         makeGameOver('1');
       } else{
         makeGameOver('2');
@@ -83,7 +83,7 @@ function Table( {makeGameOver} ) {
 function App() {
   const [gameOver, setGameOver] = useState();
   function makeGameOver(player){
-    if (player == '1'){
+    if (player === '1'){
       setGameOver("https://y.yarn.co/69daa53a-7641-45ae-aea7-203c6b4a0e90_text.gif");
     } else {
       setGameOver("https://media.tenor.com/lGhAoEiUlTcAAAAM/player2wins.gif");
